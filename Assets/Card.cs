0935fa15ce6b0950dab0ed0389ffe467cea52e2e
rleinfellner <richard@modulo17.com>
2016-10-16 17:44:00 +0100

    First Upload

---
+++ Assets/Card.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+//This script is attached to the Card Game object
+//For your workshop task you must rename the variables to use meaningful names
+//Comment the code to explain what each line is meant to do
+//Add any helpful debug code
+
+
+
+public class Card : MonoBehaviour {
+
+	public	Sprite	FrontSprite;
+	public	Sprite	BackSprite;
+
+	public	bool	ShowCard;
+
+	SpriteRenderer	SR;
+
+	void Start () {
+		SR = GetComponent<SpriteRenderer> ();	
+	}
+	
+	void Update () {
+		if (ShowCard) {
+			SR.sprite = FrontSprite;
+		} else {
+			SR.sprite = BackSprite;
+		}
+	}
+}
---
+++ Assets/RayCast.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//This script is attached to the Main Camera
+//For your workshop task you must rename the variables (tVariable) to use meaningful names reflecting what they are storing
+//Comment the code (at least 1 comment per line) to explain what each line is meant to do
+//Add any helpful debug code
+
+
+public class RayCast : MonoBehaviour {
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Ray tVariable1 = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit2D	tVariable2 = Physics2D.Raycast (tVariable1.origin,tVariable1.direction);
+			if (tVariable2.collider != null) {
+				Card tVariable3 = tVariable2.collider.gameObject.GetComponent<Card> ();
+				tVariable3.ShowCard = !tVariable3.ShowCard;
+				Debug.Log (string.Format("Ray Cast Hit {0}",tVariable3.name));
+			}
+		}
+	}
+}
