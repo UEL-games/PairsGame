968fae6d5aeb92ed1ec56f663d12f33e3a8905de
rleinfellner <richard@modulo17.com>
2016-10-18 08:11:08 +0100

    With Comments added for Workshop3

--- Assets/Card.cs
+++ Assets/Card.cs
@@ -4,30 +4,43 @@ using System.Collections;
 
 
 //This script is attached to the Card Game object
-//For your workshop task you must rename the variables to use meaningful names
-//Comment the code to explain what each line is meant to do
-//Add any helpful debug code
+//The solution file contains some ideas
+//on variable names and comments I would use
+//to describe how it works
+
+
+//Card Class
+//Used to define the behaviour of a single card
+//Assumes a card can display one of 2 sprites so it can either appear turned over (showing its back) or its Rank/Suit
 
 
 
 public class Card : MonoBehaviour {
 
-	public	Sprite	FrontSprite;
-	public	Sprite	BackSprite;
+	public	Sprite	FrontSprite;	//Sprite to use for the front of the card, assigned in IDE inspector
+	public	Sprite	BackSprite;		//Sprite to use for the back of the card, assigned in IDE inspector
 
-	public	bool	ShowCard;
+	public	bool	ShowCard;		//To Flip the card (show its value) set the ShowCard State true, also works in IDE via inspector
+									//Potential improvement, add a setter method so external code does not need to access Card variables directly
+									//Would also allow for the possibilty to not use update to check card each render, as setter could be used to update 
+									//SR.sprite when it updates the state variable
 
-	SpriteRenderer	SR;
+	SpriteRenderer	SR;				//Copy of reference to SpriteRenderer stored here,for rapid access later 
 
+
+	//Start() Unity calls this before the first Update see https://unity3d.com/learn/tutorials/topics/scripting/awake-and-start
 	void Start () {
-		SR = GetComponent<SpriteRenderer> ();	
+		SR = GetComponent<SpriteRenderer> ();	//Grab Copy of sprite renderer component
+		ShowCard=false;			//Show back of card when it starts up
 	}
-	
+
+
+	//Update() Unity calls this before every render see see https://unity3d.com/learn/tutorials/topics/scripting/awake-and-start
 	void Update () {
-		if (ShowCard) {
-			SR.sprite = FrontSprite;
+		if (ShowCard) {		//Check which side of the card to show
+			SR.sprite = FrontSprite;		//Show the front, if card does not show in game ensure FrontSprite is set in inspector.
 		} else {
-			SR.sprite = BackSprite;
+			SR.sprite = BackSprite;			//Show the back, if card does not show in game ensure BackSprite is set in inspector.
 		}
 	}
 }
--- Assets/RayCast.cs
+++ Assets/RayCast.cs
@@ -3,20 +3,33 @@ using System.Collections;
 
 
 //This script is attached to the Main Camera
-//For your workshop task you must rename the variables (tVariable) to use meaningful names reflecting what they are storing
-//Comment the code (at least 1 comment per line) to explain what each line is meant to do
-//Add any helpful debug code
+//The solution file contains some ideas
+//on variable names and comments I would use
+//to describe how it works
 
+
+//RayCast class is used to find which, if any, GameObject in the scene was clicked on
+//It starts by generating a ray from the mouse position ScreenPointToRay from the main (and only) camera in the scene
+//The ray is cast and if an GameObject (which must have a collider for this to work) is found this is acted on
 
 public class RayCast : MonoBehaviour {
+
+	//This is called every update, right now not an issue as code only runs if there was a mousedown event
+	//However beware excessive processing during Update() can really slow the game down
 	void Update () {
-		if (Input.GetMouseButtonDown (0)) {
-			Ray tVariable1 = Camera.main.ScreenPointToRay (Input.mousePosition);
-			RaycastHit2D	tVariable2 = Physics2D.Raycast (tVariable1.origin,tVariable1.direction);
-			if (tVariable2.collider != null) {
-				Card tVariable3 = tVariable2.collider.gameObject.GetComponent<Card> ();
-				tVariable3.ShowCard = !tVariable3.ShowCard;
-				Debug.Log (string.Format("Ray Cast Hit {0}",tVariable3.name));
+		if (Input.GetMouseButtonDown (0)) {			//Only run raycast if mouse down has occured since last Update
+			Ray tMousePointerRay = Camera.main.ScreenPointToRay (Input.mousePosition);		//Make a ray from the mouse pointer using the main camera
+			RaycastHit2D	tMousePointerRayHit = Physics2D.Raycast (tMousePointerRay.origin,tMousePointerRay.direction);	//Cast the ray into the gameworld from the mouse position, pointing along direction (into the screen)
+			if (tMousePointerRayHit.collider != null) {											//If collider is null we did not hit anything, otherise it wil be a reference the collider on the GameObject we hit
+				GameObject tObjectHit = tMousePointerRayHit.collider.gameObject;				//At this point we know we have hit a collider on a GameObject, so get the game object
+				Debug.Log (string.Format("Ray Cast Hit {0}",tObjectHit.name));					//Show what we have hit in the console Window, handy for Debugging
+				Card tCardHit=tObjectHit.GetComponent<Card>();									//Get the Card Script object, if it does not have one, if this returns null its either not a card, or we forgot to attach the Card Script it
+				if (tCardHit != null) {															//Ensure the object we hit is a Card, as we may have other items in the scene too later	
+					tCardHit.ShowCard = !tCardHit.ShowCard;										//Flip the card by changing its state directly
+																								//to ensure safer operation it may be better to add a card method to do this rather than set the variable here
+				} else {
+					Debug.Log (string.Format("WARNING: Ray Cast Hit Object {0} does not have a Card component",tObjectHit.GetType().Name));		//Perhaps we forgot to add one?
+				}
 			}
 		}
 	}
